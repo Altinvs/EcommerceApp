e63883ea83677a5fd0ebac4b7994ad6222643c86
Altinvs <78910193+Altinvs@users.noreply.github.com>
2022-03-02 19:15:06 +0100

    Updating a picture

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,4 +1,5 @@
 const Post = require("../models/posts")
+const fs = require("fs")
 
 module.exports = class API {
     // fetch all posts
@@ -35,7 +36,27 @@ module.exports = class API {
     }
     // Update a post
     static async updatePost(req, res){
-        res.send('Update post')
+        const id = req.params.id
+        let new_image = ''
+        if(req.file){
+            new_image = req.file.filename
+            try {
+                fs.unlinkSync('./uploads/'+req.body.old_image)
+            } catch (err) {
+                console.log(err)
+            }
+        } else {
+            new_image = req.body.old_image;
+        }
+        const newPost = req.body;
+        newPost.image = new_image;
+
+        try {
+            await Post.findByIdAndUpdate(id, newPost)
+            res.status(200).json({ message: 'Post updated sucessfully' })
+        } catch (err) {
+            res.status(404).json({ message: err.message })
+        }
     }
     // Delete a post
     static async deletePost(req, res){
