8c5589afa89d02164760a8b05e36aa172003f4f7
Altinvs <78910193+Altinvs@users.noreply.github.com>
2022-03-02 18:51:17 +0100

    posting an image

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,3 +1,38 @@
+const Post = require("../models/posts")
+
 module.exports = class API {
-    //
+    // fetch all posts
+    static async fetchAllPost(req, res){
+        try {
+            const posts = await Post.find();
+            res.status(200).json(posts);
+        } catch (err) {
+            res.status(404).json({ message: err.message })
+        }
+    }
+    // fetch fetch post by ID
+    static async fetchPostByID(req, res){
+        res.send('Fetch post by id')
+    }
+    // create a post
+    static async createPost(req, res){
+        const post = req.body;
+        const imageName = req.file.filename;
+        post.image = imageName;
+        try {
+            await Post.create(post);
+            res.status(201).json({message: 'Post created successfullly'})
+        } catch (err) {
+            res.status(400).json({message: err.message})
+            
+        }
+    }
+    // Update a post
+    static async updatePost(req, res){
+        res.send('Update post')
+    }
+    // Delete a post
+    static async deletePost(req, res){
+        res.send('Delete post')
+    }
 }
