6045571fa6b64ea1ba037dd800fc73459ef050e4
Altinvs <78910193+Altinvs@users.noreply.github.com>
2022-03-02 20:43:46 +0100

    nav routing functionality fixed

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import AddPost from '../views/AddPost'
+import Home from '../views/Home'
 
 Vue.use(VueRouter)
 
@@ -7,6 +9,12 @@ const routes = [
   {
     path: '/',
     name: 'home',
+    component: Home,
+  },
+  {
+    path: '/add-post',
+    name: 'add-post',
+    component: AddPost,
   },
   {
     path: '/about',
@@ -14,7 +22,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
