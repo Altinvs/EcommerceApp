615275598e201cd1e3d8a7cdbe010efc55337e94
Altinvs <78910193+Altinvs@users.noreply.github.com>
2022-03-13 16:43:42 +0100

    login and route protection #1

--- client/src/Warehouse/Auth.js
+++ client/src/Warehouse/Auth.js
@@ -14,15 +14,39 @@ const getters = {
 };
 
 const actions = {
+    //login
+    async login({
+        commit
+    }, user){
+        commit('auth_request');
+        let res = await axios.post('http://localhost:5000/api/users/login', user)
+        if(res.data.success){
+            const token = res.data.token;
+            const user = res.data.user;
+            //store token into local storage
+            localStorage.setItem('token', token);
+            //set axios defaults
+            axios.defaults.headers.common['Authorization'] = token;
+            commit('auth_success', token, user);
+        }
+        return res;
+    }
 
 };
 
 const mutations = {
-
+    auth_request(state){
+        state.status = 'loading'
+    },
+    auth_success(state, token, user){
+        state.token = token
+        state.user = user
+        state.status = 'success'
+    }
 };
 
+
 export default {
-    namespaced: true,
     state,
     getters,
     actions,
